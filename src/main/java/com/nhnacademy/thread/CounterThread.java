a2adc35eb7237077afac0656784801774eef7020
00dbgpdnjs <00dbgpdnjs@naver.com>
2024-09-10 13:43:33 +0900

    Finish step1-2 counter-by-thread

--- src/main/java/com/nhnacademy/thread/CounterThread.java
+++ src/main/java/com/nhnacademy/thread/CounterThread.java
@@ -18,16 +18,17 @@ import java.util.Objects;
 
 @Slf4j
 //TODO#1 CounterThread는 Thread를 상속 합니다.
-public class CounterThread {
+public class CounterThread extends Thread{
     private final long countMaxSize;
 
     private long count;
 
     public CounterThread(String name, long countMaxSize) {
         //TODO#2 name <-- null 이거나 공백 문자열이면 IllegalArgumentException이 발생 합니다.
+        if(name == null || name.isEmpty()) throw new IllegalArgumentException();
 
         //TODO#3 countMaxSize <=0 이면 IllegalArgumentException이 발생 합니다.
-
+        if (countMaxSize <= 0) throw new IllegalArgumentException();
 
         this.setName(name);
         this.countMaxSize = countMaxSize;
@@ -46,6 +47,13 @@ public class CounterThread {
          */
 
         do {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                this.interrupt();
+            }
+            count++;
+            log.info("thread:" + this.getName() + ", count:" + count);
 
         }while (count<countMaxSize);
     }
